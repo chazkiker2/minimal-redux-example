ec149907aeab06bff87c566f8b136d04bfa1614b
Dima Parzhitsky <parzhitsky@gmail.com>
2020-08-25 01:24:16 +0300

    Provide type for `action` reducer parameter

--- src/store/user/types.ts
+++ src/store/user/types.ts
@@ -1,5 +1,3 @@
-import type * as actionTypes from "./action-types";
-
 export interface State {
 	name: string | null;
 	age: number | null;
@@ -10,26 +8,3 @@ export interface PayloadMap {
 	USER$SET_AGE: number;
 	USER$CLEAR: never;
 }
-
-export interface ActionSetName {
-	type: typeof actionTypes.SET_NAME;
-	payload: string;
-}
-
-export interface ActionSetAge {
-	type: typeof actionTypes.SET_AGE;
-	payload: number;
-}
-
-export interface ActionClear {
-	type: typeof actionTypes.CLEAR;
-}
-
-// TODO: other action interfaces
-
-export type Action =
-	| ActionSetName
-	| ActionSetAge
-	| ActionClear
-	// TODO: other actions
-	;
